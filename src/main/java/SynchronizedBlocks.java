d20895f1db154a563b2320b7497c1e2adc9cff4c
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-27 16:16:06 +0300

    Add eaxample, where syncronized method blocks all other methods for class

---
+++ src/main/java/SynchronizedBlocks.java
@@ -0,0 +1,74 @@
+import java.util.*;
+public class SynchronizedBlocks {
+    private int[] a = {0, 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10};
+    private int[] b = {0, 11, 12, 13, 14, 15, 16, 17, 18 ,19, 20};
+
+    private List<Integer> intList1 = new ArrayList<>();
+    private List<Integer> intList2 = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        SynchronizedBlocks sb = new SynchronizedBlocks();
+        sb.copy();
+    }
+
+    private void copy(){
+        long start = System.currentTimeMillis();
+        Thread thread1 = new Thread(new RunnerA());
+        Thread thread2 = new Thread(new RunnerB());
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("List has taken " + (end-start) + " milliseconds");
+    }
+
+    private synchronized void copyArrayA() {
+
+        for(int i = 0; i < a.length; i++){
+            intList1.add(a[i]);
+            System.out.println(intList1);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void copyArrayB() {
+        for(int i = 0; i < b.length; i++){
+            intList2.add(b[i]);
+            System.out.println(intList2);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private class RunnerA implements Runnable{
+
+        @Override
+        public void run() {
+            copyArrayA();
+        }
+    }
+
+    private class RunnerB implements Runnable{
+
+        @Override
+        public void run() {
+            copyArrayB();
+        }
+    }
+}
